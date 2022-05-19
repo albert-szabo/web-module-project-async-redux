95e6ca1865a762a9a2e4aa168a9ee7ca6a1f2e5d
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-18 21:15:50 -0700

    Added scaffolding to prepare for receiving data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import UserList from './components/UserList';
+
 function App() {
   return (
     <div className="App">
-      Random User Generator Display Project
+      <h1>Random User Generator Display Project</h1>
+      <UserList users={users} />
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const User = (props) => {
+    const { user } = props;
+
+    return (<div id='user'>
+        <h3>{user.name.title}{user.name.first}{user.name.last}</h3>
+        <h4>{user.email}</h4>
+        <h5>Username: {user.login.username}</h5>
+        <h5>Password: {user.login.password}</h5>
+        <h6>Age: {user.dob.age}</h6>
+        <p>{user.phone}</p>
+        <p>{user.cell}</p>
+        <img src={user.picture.large}/>
+    </div>);
+}
+
+export default User;
